b6eb4d15273cc054f74b2dc9e677db8da8eb9714
birchie liu <56035255+teenkit@users.noreply.github.com>
2019-11-17 14:41:24 +0800

    update to Chinese comments
    set block description to Chinese Simplified

--- ds1307.ts
+++ ds1307.ts
@@ -1,41 +1,40 @@
 /**
- * makecode DS1307 RTC Package.
- * From microbit/micropython Chinese community.
- * http://www.micropython.org.cn
+ * makecode DS1388 RTC Package.
+ * 
  */
 
 /**
- * DS1307 block
+ * DS1388 block
  */
-//% weight=20 color=#8010f0 icon="\uf017" block="DS1307"
-namespace DS1307 {
-    let DS1307_I2C_ADDR = 104;
-    let DS1307_REG_SECOND = 0
-    let DS1307_REG_MINUTE = 1
-    let DS1307_REG_HOUR = 2
-    let DS1307_REG_WEEKDAY = 3
-    let DS1307_REG_DAY = 4
-    let DS1307_REG_MONTH = 5
-    let DS1307_REG_YEAR = 6
-    let DS1307_REG_CTRL = 7
-    let DS1307_REG_RAM = 8
+//% weight=20 color=#8010f0 icon="\uf017" block="TEENKIT时钟"
+namespace DS1388 {
+    let DS1388_I2C_ADDR = 104;
+    let DS1388_REG_SECOND = 0
+    let DS1388_REG_MINUTE = 1
+    let DS1388_REG_HOUR = 2
+    let DS1388_REG_WEEKDAY = 3
+    let DS1388_REG_DAY = 4
+    let DS1388_REG_MONTH = 5
+    let DS1388_REG_YEAR = 6
+    let DS1388_REG_CTRL = 7
+    let DS1388_REG_RAM = 8
 
     /**
-     * set ds1307's reg
+     * set ds1388's reg
      */
     function setReg(reg: number, dat: number): void {
         let buf = pins.createBuffer(2);
         buf[0] = reg;
         buf[1] = dat;
-        pins.i2cWriteBuffer(DS1307_I2C_ADDR, buf);
+        pins.i2cWriteBuffer(DS1388_I2C_ADDR, buf);
     }
 
     /**
-     * get ds1307's reg
+     * get ds1388's reg
      */
     function getReg(reg: number): number {
-        pins.i2cWriteNumber(DS1307_I2C_ADDR, reg, NumberFormat.UInt8BE);
-        return pins.i2cReadNumber(DS1307_I2C_ADDR, NumberFormat.UInt8BE);
+        pins.i2cWriteNumber(DS1388_I2C_ADDR, reg, NumberFormat.UInt8BE);
+        return pins.i2cReadNumber(DS1388_I2C_ADDR, NumberFormat.UInt8BE);
     }
 
     /**
@@ -53,196 +52,179 @@ namespace DS1307 {
     }
 
     /**
-     * start ds1307 (go on)
+     * 启动时钟
      */
-    //% blockId="DS1307_START" block="start"
+    //% blockId="DS1388_START" block="开始"
     //% weight=52 blockGap=8 
-    //% parts=DS1307 trackArgs=0
     export function start() {
         let t = getSecond()
         setSecond(t & 0x7f)
     }
 
     /**
-     * stop ds1307 (pause)
+     * 暂停时钟
      */
-    //% blockId="DS1307_STOP" block="pause"
+    //% blockId="DS1388_STOP" block="暂停"
     //% weight=51 blockGap=8 
-    //% parts=DS1307 trackArgs=0
     export function stop() {
         let t = getSecond()
         setSecond(t | 0x80)
     }
 
     /**
-     * get Year
+     * 读取年份
      */
-    //% blockId="DS1307_GET_YEAR" block="year"
+    //% blockId="DS1388_GET_YEAR" block="年"
     //% weight=99 blockGap=8 
-    //% parts=DS1307 trackArgs=0
     export function getYear(): number {
-        return (HexToDec(getReg(DS1307_REG_YEAR)) + 2000)
+        return (HexToDec(getReg(DS1388_REG_YEAR)) + 2000)
     }
 
     /**
-     * set year
-     * @param dat is the Year will be set, eg: 2018
+     * 设置年份
+     * @param dat is the Year will be set, eg: 2019
      */
-    //% blockId="DS1307_SET_YEAR" block="set year %dat"
+    //% blockId="DS1388_SET_YEAR" block="设置 年 %dat"
     //% weight=69 blockGap=8 
-    //% parts=DS1307 trackArgs=0
     export function setYear(dat: number): void {
-        setReg(DS1307_REG_YEAR, DecToHex(dat % 100))
+        setReg(DS1388_REG_YEAR, DecToHex(dat % 100))
     }
 
     /**
-     * get Month
+     * 读取月份
      */
-    //% blockId="DS1307_GET_MONTH" block="month"
+    //% blockId="DS1388_GET_MONTH" block="月"
     //% weight=98 blockGap=8 
-    //% parts=DS1307 trackArgs=0
     export function getMonth(): number {
-        return HexToDec(getReg(DS1307_REG_MONTH))
+        return HexToDec(getReg(DS1388_REG_MONTH))
     }
 
     /**
-     * set month
-     * @param dat is Month will be set.  eg: 2
+     * 设置月份
+     * @param dat is Month will be set.  eg: 11
      */
-    //% blockId="DS1307_SET_MONTH" block="set month %dat"
+    //% blockId="DS1388_SET_MONTH" block="设置 月份 %dat"
     //% weight=68 blockGap=8
     //% dat.min=1 dat.max=12
-    //% parts=DS1307 trackArgs=0
     export function setMonth(dat: number): void {
-        setReg(DS1307_REG_MONTH, DecToHex(dat % 13))
+        setReg(DS1388_REG_MONTH, DecToHex(dat % 13))
     }
 
     /**
-     * get Day
+     * 读取日
      */
-    //% blockId="DS1307_GET_DAY" block="day"
+    //% blockId="DS1388_GET_DAY" block="日"
     //% weight=97 blockGap=8 
-    //% parts=DS1307 trackArgs=0
     export function getDay(): number {
-        return HexToDec(getReg(DS1307_REG_DAY))
+        return HexToDec(getReg(DS1388_REG_DAY))
     }
 
     /**
-     * set day
-     * @param dat is the Day will be set, eg: 15
+     * 设置日
+     * @param dat is the Day will be set, eg: 17
      */
-    //% blockId="DS1307_SET_DAY" block="set day %dat"
+    //% blockId="DS1388_SET_DAY" block="设置 日 %dat"
     //% weight=67 blockGap=8
     //% dat.min=1 dat.max=31
-    //% parts=DS1307 trackArgs=0
     export function setDay(dat: number): void {
-        setReg(DS1307_REG_DAY, DecToHex(dat % 32))
+        setReg(DS1388_REG_DAY, DecToHex(dat % 32))
     }
 
     /**
-     * get Week Day
+     * 读取星期几
      */
-    //% blockId="DS1307_GET_WEEKDAY" block="weekday"
+    //% blockId="DS1388_GET_WEEKDAY" block="星期"
     //% weight=96 blockGap=8 
-    //% parts=DS1307 trackArgs=0
     export function getWeekday(): number {
-        return HexToDec(getReg(DS1307_REG_WEEKDAY))
+        return HexToDec(getReg(DS1388_REG_WEEKDAY))
     }
 
     /**
-     * set weekday
-     * @param dat is the Week Day will be set, eg: 4
+     * 设置星期几
+     * @param dat is the Week Day(1-7) will be set, eg: 4
      */
-    //% blockId="DS1307_SET_WEEKDAY" block="set weekday %dat"
+    //% blockId="DS1388_SET_WEEKDAY" block="设置 星期 %dat"
     //% weight=66 blockGap=8
     //% dat.min=1 dat.max=7
-    //% parts=DS1307 trackArgs=0
     export function setWeekday(dat: number): void {
-        setReg(DS1307_REG_WEEKDAY, DecToHex(dat % 8))
+        setReg(DS1388_REG_WEEKDAY, DecToHex(dat % 8))
     }
 
     /**
-     * get Hour
+     * 读取小时
      */
-    //% blockId="DS1307_GET_HOUR" block="hour"
+    //% blockId="DS1388_GET_HOUR" block="小时"
     //% weight=95 blockGap=8 
-    //% parts=DS1307 trackArgs=0
     export function getHour(): number {
-        return HexToDec(getReg(DS1307_REG_HOUR))
+        return HexToDec(getReg(DS1388_REG_HOUR))
     }
 
     /**
-     * set hour
+     * 设置小时
      * @param dat is the Hour will be set, eg: 0
      */
-    //% blockId="DS1307_SET_HOUR" block="set hour %dat"
+    //% blockId="DS1388_SET_HOUR" block="设置 小时 %dat"
     //% weight=65 blockGap=8
     //% dat.min=0 dat.max=23
-    //% parts=DS1307 trackArgs=0
     export function setHour(dat: number): void {
-        setReg(DS1307_REG_HOUR, DecToHex(dat % 24))
+        setReg(DS1388_REG_HOUR, DecToHex(dat % 24))
     }
 
     /**
-     * get Minute
+     * 读取分钟
      */
-    //% blockId="DS1307_GET_MINUTE" block="minute"
+    //% blockId="DS1388_GET_MINUTE" block="分钟"
     //% weight=94 blockGap=8 
-    //% parts=DS1307 trackArgs=0
     export function getMinute(): number {
-        return HexToDec(getReg(DS1307_REG_MINUTE))
+        return HexToDec(getReg(DS1388_REG_MINUTE))
     }
 
     /**
-     * set minute
+     * 设置分钟
      * @param dat is the Minute will be set, eg: 0
      */
-    //% blockId="DS1307_SET_MINUTE" block="set minute %dat"
+    //% blockId="DS1388_SET_MINUTE" block="设置 分钟 %dat"
     //% weight=64 blockGap=8
     //% dat.min=0 dat.max=59
-    //% parts=DS1307 trackArgs=0
     export function setMinute(dat: number): void {
-        setReg(DS1307_REG_MINUTE, DecToHex(dat % 60))
+        setReg(DS1388_REG_MINUTE, DecToHex(dat % 60))
     }
 
     /**
-     * get Second
+     * 读取秒
      */
-    //% blockId="DS1307_GET_SECOND" block="second"
+    //% blockId="DS1388_GET_SECOND" block="秒"
     //% weight=93 blockGap=8 
-    //% parts=DS1307 trackArgs=0
     export function getSecond(): number {
-        return HexToDec(getReg(DS1307_REG_SECOND))
+        return HexToDec(getReg(DS1388_REG_SECOND))
     }
 
     /**
-     * set second
+     * 设置秒
      * @param dat is the Second will be set, eg: 0
      */
-    //% blockId="DS1307_SET_SECOND" block="set second %dat"
+    //% blockId="DS1388_SET_SECOND" block="设置 秒 %dat"
     //% weight=63 blockGap
     //% dat.min=0 dat.max=59
-    //% parts=DS1307 trackArgs=0
     export function setSecond(dat: number): void {
-        setReg(DS1307_REG_SECOND, DecToHex(dat % 60))
+        setReg(DS1388_REG_SECOND, DecToHex(dat % 60))
     }
 
     /**
-     * set Date and Time
-     * @param year is the Year will be set, eg: 2018
-     * @param month is the Month will be set, eg: 2
-     * @param day is the Day will be set, eg: 15
-     * @param weekday is the Weekday will be set, eg: 4
-     * @param hour is the Hour will be set, eg: 0
-     * @param minute is the Minute will be set, eg: 0
-     * @param second is the Second will be set, eg: 0
+     * 初始化时钟的日期和时间
+     * @param year is the Year will be set, eg: 2019
+     * @param month is the Month will be set, eg: 11
+     * @param day is the Day will be set, eg: 17
+     * @param weekday is the Weekday will be set, eg: 1
+     * @param hour is the Hour will be set, eg: 8
+     * @param minute is the Minute will be set, eg: 30
+     * @param second is the Second will be set, eg: 59
      */
-    //% blockId="DS1307_SET_DATETIME" block="set year %year|month %month|day %day|weekday %weekday|hour %hour|minute %minute|second %second"
+    //% blockId="DS1388_SET_DATETIME" block="设置 年 %year|月 %month|日 %day|星期 %weekday|时 %hour|分 %minute|秒 %second"
     //% weight=60 blockGap
-    //% parts=DS1307 trackArgs=0
     export function DateTime(year: number, month: number, day: number, weekday: number, hour: number, minute: number, second: number): void {
         let buf = pins.createBuffer(8);
-        buf[0] = DS1307_REG_SECOND;
+        buf[0] = DS1388_REG_SECOND;
         buf[1] = DecToHex(second % 60);
         buf[2] = DecToHex(minute % 60);
         buf[3] = DecToHex(hour % 24);
@@ -250,7 +232,7 @@ namespace DS1307 {
         buf[5] = DecToHex(day % 32);
         buf[6] = DecToHex(month % 13);
         buf[7] = DecToHex(year % 100);
-        pins.i2cWriteBuffer(DS1307_I2C_ADDR, buf)
+        pins.i2cWriteBuffer(DS1388_I2C_ADDR, buf)
     }
 
 }
