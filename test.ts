ba7fab09035a4b45bcdef2aef8d37dff88f373fc
birchie liu <56035255+teenkit@users.noreply.github.com>
2019-11-18 16:02:34 +0800

    create
    create

--- test.ts
+++ test.ts
@@ -1,8 +1,8 @@
 input.onButtonPressed(Button.A, () => {
-    DS1307.setSecond(0)
+    DS1388.setSecond(0)
 })
-DS1307.start()
+DS1388.start()
 basic.forever(() => {
-    basic.showNumber(DS1307.getSecond() % 10)
+    basic.showNumber(DS1388.getSecond() % 10)
     basic.pause(100)
 })
